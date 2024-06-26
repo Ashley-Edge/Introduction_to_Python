f0945b9a43a987542ef31c977f6c5f9dbb393456
Ashley <ashley@akoova.com>
2024-06-26 11:54:05 +0100

    Adding script from HW question 1

---
+++ HW1_Chair_Nails.py
@@ -0,0 +1,33 @@
+# Homework: Session 1
+
+# Question 1
+# I am building some very high quality chairs and need exactly four nails for each chair.
+# I've written a program to calculate how many nails I need to buy to build these chairs.
+
+# chairs = '15' nails = 4
+# total_nails = chairs * nails
+# message = 'I need to buy {} nails'.format(total_nails) print('You need to buy {} nails'.format(message))
+# When I run the program it tells me that I need to buy 15151515 nails. This seems like a lot of nails.
+# Is my program calculating the total number of nails correctly? What is the problem? How do I fix it?
+
+# The Original Script with issues
+
+# chairs = '15' nails = 4
+# total_nails = chairs * nails
+# message = 'I need to buy {} nails'.format(total_nails) print('You need to buy {} nails'.format(message))
+
+# Problems
+# 15 is in ' ' turing it into a string
+# The nails variable should be on a new line
+# print() needs to be on a new line too
+# Output is 'You need to buy I need to buy 60 nails nails'
+# print() Should just be calling the message
+# I have updated the message too
+
+chairs = 15
+nails = 4
+total_nails = chairs * nails
+message = 'I need to buy {} nails to build {} chairs'.format(total_nails, chairs)
+print(message)
+
+# 'I need to buy 60 nails to build 15 chairs'
